78778120417f66b8b4a99c49f2b6bc7443e28937
Haris Sarki <sarkiharish42@gmail.com>
2023-11-29 21:04:40 +0530

    register controller bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         //file has been uploaded successfully
-        console.log("file is uploaded successfully", response.url);
+        //console.log("file is uploaded successfully", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
